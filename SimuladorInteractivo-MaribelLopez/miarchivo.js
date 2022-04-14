6ef4ac920095f961ff7188c4bf54de98c398d659
maricrack <100219247+maricrack@users.noreply.github.com>
2022-04-14 13:35:33 -0500

    Update miarchivo.js

--- SimuladorInteractivo-MaribelLopez/miarchivo.js
+++ SimuladorInteractivo-MaribelLopez/miarchivo.js
@@ -1,20 +1,28 @@
 let ingresaNombre = prompt("Ingrese el nombre del estudiante");
 let ingresaApellido = prompt("Ingrese el apellido del estudiante");
 {
-    alert ("El nombre del estudiantes es " + ingresaNombre  +"\nApellido: "+ ingresaApellido);
+    alert ("El nombre del estudiantes es " + ingresaNombre  +"\n El Apellido del estudiante es: "+ ingresaApellido);
 }
 {
 let nota1 = parseFloat (prompt("Ingrese la nota del primer corte"));
 let nota2 = parseFloat (prompt("Ingrese la nota del segundo corte"));
 let nota3 = parseFloat (prompt("Ingrese la nota del tercer corte"));
-let resultado = parseFloat ((nota1 + nota2 + nota3)/3);
-alert ("La nota final es " + resultado);
-if(resultado<3)
+
+function promedio ()
 {
+
+    let resultado = parseFloat (nota1 + nota2 + nota3);
+    let resultado1 = parseFloat (resultado/3);
+    alert ("La nota final es " + resultado1);
+    
+    if(resultado1<3)
+    {
     alert("De acuerdo al promedio el estudiante REPROBÓ");
+    }
+        else 
+        {
+            alert("De acuerdo al promedio el estudiante APROBÓ");
+        }
 }
-else 
-{
-    alert("De acuerdo al promedio el estudiante APROBÓ");
-}
+promedio();  
 }
