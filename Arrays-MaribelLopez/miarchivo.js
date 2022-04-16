174b4b31851bf47ad06dcc4b864a91e30c8fc61b
maricrack <100219247+maricrack@users.noreply.github.com>
2022-04-16 17:40:14 -0500

    Update miarchivo.js

--- Arrays-MaribelLopez/miarchivo.js
+++ Arrays-MaribelLopez/miarchivo.js
@@ -1,20 +1,40 @@
-let ingresaNombre = prompt("Ingrese el nombre del estudiante");
-let ingresaApellido = prompt("Ingrese el apellido del estudiante");
-{
-    alert ("El nombre del estudiantes es " + ingresaNombre  +"\nApellido: "+ ingresaApellido);
+    for (let repetir=0; repetir<2; repetir++)
+    {
+    let ingresaNombre = prompt("Ingrese el nombre del estudiante");
+    let ingresaApellido = prompt("Ingrese el apellido del estudiante");
+    {
+    let nota1 = parseFloat (prompt("Ingrese la nota del primer corte"));
+    let nota2 = parseFloat (prompt("Ingrese la nota del segundo corte"));
+    let nota3 = parseFloat (prompt("Ingrese la nota del tercer corte"));
+
+    function promedio ()
+    {
+        let resultado = parseFloat (nota1 + nota2 + nota3);
+        let resultado1 = parseFloat (resultado/3);
+        alert ("La nota final es " + resultado1);
+    
+        if(resultado1<3)
+        {
+        alert("De acuerdo al promedio el estudiante REPROBÓ");
+        }
+        else 
+        {
+            alert("De acuerdo al promedio el estudiante APROBÓ");
+        }
+    }
+    promedio();  
 }
-{
-let nota1 = parseFloat (prompt("Ingrese la nota del primer corte"));
-let nota2 = parseFloat (prompt("Ingrese la nota del segundo corte"));
-let nota3 = parseFloat (prompt("Ingrese la nota del tercer corte"));
-let resultado = parseFloat ((nota1 + nota2 + nota3)/3);
-alert ("La nota final es " + resultado);
-if(resultado<=3)
-{
-    alert("De acuerdo al promedio el estudiante REPROBÓ");
+function mostrarNombre()
+{    
+    const nombres =[]
+    const apellido=[]
+    
+    nombres.push(ingresaNombre);
+    apellido.push(ingresaApellido);
+       
+    alert ("Cantidad de datos ingresados son " + nombres.length);
+    const completo = nombres.concat(apellido);
+    alert (completo);    
 }
-else 
-{
-    alert("De acuerdo al promedio el estudiante APROBÓ");
-}
+mostrarNombre();
 }
